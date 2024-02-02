3a4fabf0af1ce62ceba7dd3e4f6b2d0750e5d244
Mostafa <nonatefa11@gmail.com>
2024-02-03 00:51:20 +0200

    fetch all vendor Info in Perconal Info Screen

--- lib/app/views/screens/perconal_Info_Screen/widgets/PerconalInfoScreenBody.dart
+++ lib/app/views/screens/perconal_Info_Screen/widgets/PerconalInfoScreenBody.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:nike_app_vendors/app/data/Cubits/vendor_cubit/vendor_cubit.dart';
 import '../../../../core/constants.dart';
 import '../../../../core/styles/App_Colors.dart';
 import '../../../../core/styles/text_Style.dart';
+import '../../../../core/tools/reg_imp.dart';
 import '../../../widgets/CustomTextFormField.dart';
 import '../../../widgets/VsizedBox.dart';
 import '../../../widgets/customMainButton.dart';
@@ -16,95 +16,104 @@ class PerconalInfoScreenBody extends StatelessWidget {
     return SafeArea(
         child: Padding(
       padding: EdgeInsets.only(top: 15.h, left: 14.w, right: 14.w),
-      child: SingleChildScrollView(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-              child: Stack(
-            children: [
-              CircleAvatar(
-                radius: 50.r,
-              ),
-              Positioned(
-                bottom: 0,
-                right: 0,
-                child: CircleAvatar(
-                  radius: 13.r,
-                  backgroundColor: AppColors.kPrimaryColor,
-                  child: IconButton(
-                      onPressed: () {
-                        //      GoRouter.of(context).push(Approuter.editprofilescreen);
-                      },
-                      icon: Icon(
-                        FontAwesomeIcons.pen,
-                        size: 10.sp,
-                        color: Colors.white,
-                      )),
+      child: SingleChildScrollView(child: BlocBuilder<VendorCubit, VendorState>(
+        builder: (context, state) {
+          if (state is VendorInfoSuccsess) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                    child: Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 65.r,
+                      backgroundImage:
+                          NetworkImage(state.vendorModel.vendorImageUrl!),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 8.w,
+                      child: CircleAvatar(
+                        radius: 13.r,
+                        backgroundColor: AppColors.kPrimaryColor,
+                        child: IconButton(
+                            onPressed: () {
+                              //      GoRouter.of(context).push(Approuter.editprofilescreen);
+                            },
+                            icon: Icon(
+                              FontAwesomeIcons.pen,
+                              size: 10.sp,
+                              color: Colors.white,
+                            )),
+                      ),
+                    )
+                  ],
+                )),
+                const VsizedBox(height: 30),
+                Text(
+                  "Your Name",
+                  style: Txtstyle.style16(context: context).copyWith(
+                      color: AppColors.kGreyColorB81,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: Constants.relwayFamily),
                 ),
-              )
-            ],
-          )),
-          const VsizedBox(height: 30),
-          Text(
-            "Your Name",
-            style: Txtstyle.style16(context: context).copyWith(
-                color: AppColors.kGreyColorB81,
-                fontWeight: FontWeight.w500,
-                fontFamily: Constants.relwayFamily),
-          ),
-          const VsizedBox(height: 8),
-          CustomTextFormField(
-            enableWriting: false,
-            hinttext: "Mostafa Yasser",
-            fontcolor: AppColors.kFontColor,
-            securPass: false,
-            width: double.infinity,
-            height: 80.h,
-          ),
-          const VsizedBox(height: 12),
-          Text(
-            "Email Address",
-            style: Txtstyle.style16(context: context).copyWith(
-                color: AppColors.kGreyColorB81,
-                fontWeight: FontWeight.w500,
-                fontFamily: Constants.relwayFamily),
-          ),
-          const VsizedBox(height: 8),
-          CustomTextFormField(
-            enableWriting: false,
-            hinttext: "XYZ@gmail.com",
-            fontcolor: AppColors.kFontColor,
-            securPass: false,
-            width: double.infinity,
-            height: 80.h,
-          ),
-          const VsizedBox(height: 12),
-          Text(
-            "Mobile Number",
-            style: Txtstyle.style16(context: context).copyWith(
-                color: AppColors.kGreyColorB81,
-                fontWeight: FontWeight.w500,
-                fontFamily: Constants.relwayFamily),
-          ),
-          const VsizedBox(height: 8),
-          CustomTextFormField(
-            enableWriting: false,
-            hinttext: "01289880177",
-            securPass: false,
-            fontcolor: AppColors.kFontColor,
-            width: double.infinity,
-            height: 80.h,
-          ),
-          const VsizedBox(height: 8),
-          CustomMainButton(
-            color: AppColors.kPrimaryColor,
-            fcolorWhite: true,
-            width: 375.w,
-            txt: "Save Now",
-            onPressed: () {},
-          ),
-        ],
+                const VsizedBox(height: 8),
+                CustomTextFormField(
+                  enableWriting: false,
+                  hinttext: state.vendorModel.name!,
+                  fontcolor: AppColors.kFontColor,
+                  securPass: false,
+                  width: double.infinity,
+                  height: 80.h,
+                ),
+                const VsizedBox(height: 12),
+                Text(
+                  "Email Address",
+                  style: Txtstyle.style16(context: context).copyWith(
+                      color: AppColors.kGreyColorB81,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: Constants.relwayFamily),
+                ),
+                const VsizedBox(height: 8),
+                CustomTextFormField(
+                  enableWriting: false,
+                  hinttext: state.vendorModel.email!,
+                  fontcolor: AppColors.kFontColor,
+                  securPass: false,
+                  width: double.infinity,
+                  height: 80.h,
+                ),
+                const VsizedBox(height: 12),
+                Text(
+                  "Mobile Number",
+                  style: Txtstyle.style16(context: context).copyWith(
+                      color: AppColors.kGreyColorB81,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: Constants.relwayFamily),
+                ),
+                const VsizedBox(height: 8),
+                CustomTextFormField(
+                  enableWriting: false,
+                  hinttext: state.vendorModel.number!.toString(),
+                  securPass: false,
+                  fontcolor: AppColors.kFontColor,
+                  width: double.infinity,
+                  height: 80.h,
+                ),
+                const VsizedBox(height: 30),
+                CustomMainButton(
+                  color: AppColors.kPrimaryColor,
+                  fcolorWhite: true,
+                  width: 375.w,
+                  txt: "Save Now",
+                  onPressed: () {},
+                ),
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       )),
     ));
   }
